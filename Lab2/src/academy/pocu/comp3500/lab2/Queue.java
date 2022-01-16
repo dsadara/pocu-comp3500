f0d122ca62fe91f826d40b3c81e1d29a1f525a38
DESKTOP-2K1CPFV\lim97 <lim971230@yonsei.ac.kr>
2022-01-16 18:16:35 +0900

    Lab2 Queue, Stack 구현

--- Lab2/src/academy/pocu/comp3500/lab2/Queue.java
+++ Lab2/src/academy/pocu/comp3500/lab2/Queue.java
@@ -3,19 +3,35 @@ package academy.pocu.comp3500.lab2;
 import academy.pocu.comp3500.lab2.datastructure.Node;
 
 public final class Queue {
-    public void enqueue(final int data) {
+    private Node queueList;
+    private int queueSize;
 
+    public Queue() {
+        queueSize = 0;
+    }
+
+    public void enqueue(final int data) {
+        if (queueList == null) {
+            queueList = new Node(data);
+            queueSize++;
+            return;
+        }
+        queueList = LinkedList.append(queueList, data);
+        queueSize++;
     }
 
     public int peek() {
-        return -1;
+        return queueList.getData();
     }
 
     public int dequeue() {
-        return -1;
+        int removedData = queueList.getData();
+        queueList = LinkedList.removeAt(queueList, 0);
+        queueSize--;
+        return removedData;
     }
 
     public int getSize() {
-        return -1;
+        return queueSize;
     }
 }
--- Lab2/src/academy/pocu/comp3500/lab2/Stack.java
+++ Lab2/src/academy/pocu/comp3500/lab2/Stack.java
@@ -3,19 +3,36 @@ package academy.pocu.comp3500.lab2;
 import academy.pocu.comp3500.lab2.datastructure.Node;
 
 public final class Stack {
-    public void push(final int data) {
+    private Node stackList;
+    private int stackSize;
 
+    public Stack() {
+        stackSize = 0;
+    }
+
+    public void push(final int data) {
+        if (stackList == null) {
+            stackList = new Node(data);
+            stackSize++;
+            return;
+        }
+        stackList = LinkedList.prepend(stackList, data);
+        stackSize++;
     }
 
     public int peek() {
-        return -1;
+        return stackList.getData();
     }
 
     public int pop() {
-        return -1;
+        int removedData = stackList.getData();
+        stackList = LinkedList.removeAt(stackList, 0);
+        stackSize--;
+        return removedData;
     }
 
+
     public int getSize() {
-        return -1;
+        return stackSize;
     }
 }
