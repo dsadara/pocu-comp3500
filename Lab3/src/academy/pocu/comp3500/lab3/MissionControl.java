96423463e38bbcbcffba398fa76d5165ff841bf1
DESKTOP-2K1CPFV\lim97 <lim971230@yonsei.ac.kr>
2022-01-23 14:22:01 +0900

    Ascending descending 최적화

--- Lab3/src/academy/pocu/comp3500/lab3/MissionControl.java
+++ Lab3/src/academy/pocu/comp3500/lab3/MissionControl.java
@@ -7,22 +7,31 @@ public final class MissionControl {
     }
 
     public static int findMaxAltitudeTime(final int[] altitudes) {
-        int maxAltitude = Integer.MIN_VALUE;
-        int maxAltitudeIndex = -1;
-        for (int i = 0; i < altitudes.length; i++) {
-            int currAltitude = altitudes[i];
-            if (currAltitude > maxAltitude) {
-                maxAltitudeIndex = i;
-                maxAltitude = currAltitude;
-            }
+//        int maxAltitude = Integer.MIN_VALUE;
+//        int maxAltitudeIndex = -1;
+//        for (int i = 0; i < altitudes.length; i++) {
+//            int currAltitude = altitudes[i];
+//            if (currAltitude > maxAltitude) {
+//                maxAltitudeIndex = i;
+//                maxAltitude = currAltitude;
+//            }
+//
+//        }
+//        return maxAltitudeIndex;
 
+        int flightTime = altitudes.length;
+        if (altitudes[0] + (flightTime - 1) <= altitudes[flightTime - 1] ) {   // 고도가 올라가기만 하는 경우
+            return flightTime - 1;
         }
-        return maxAltitudeIndex;
+        if (altitudes[0] - (flightTime - 1) >= altitudes[flightTime - 1]) {    // 고도가 내려가기만 하는 경우
+            return 0;
+        }
+        return -1;
     }
 
     public static ArrayList<Integer> findAltitudeTimes(final int[] altitudes, final int targetAltitude) {
         ArrayList<Integer> altitudeTimes = new ArrayList<Integer>();
-        for (int i = 0; i < altitudes.length ; i++) {
+        for (int i = 0; i < altitudes.length; i++) {
             if (altitudes[i] == targetAltitude) {
                 altitudeTimes.add(i);
             }
