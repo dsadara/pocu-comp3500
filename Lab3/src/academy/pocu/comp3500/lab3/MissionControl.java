9f7c87734bbf93ffe5df8340103dc6ea61227ef5
DESKTOP-2K1CPFV\lim97 <lim971230@yonsei.ac.kr>
2022-01-23 16:18:35 +0900

    altitudes 원소가 2개인 경우 해결

--- Lab3/src/academy/pocu/comp3500/lab3/MissionControl.java
+++ Lab3/src/academy/pocu/comp3500/lab3/MissionControl.java
@@ -19,6 +19,12 @@ public final class MissionControl {
 //
 //        }
 //        return maxAltitudeIndex;
+        int searchResult = -1;
+        if (altitudes.length != 2) {    // altitudes 원소의 개수가 2개일때는 최댓값 search 하지않음
+            searchResult = BinarySearch.binarySearch(altitudes);    // 올라갔다 내려가는경우 최대고도 찾기
+            if (searchResult != -1)
+                return searchResult;
+        }
         int flightTime = altitudes.length;
         if (altitudes[0] + (flightTime - 1) <= altitudes[flightTime - 1] ) {   // 고도가 올라가기만 하는 경우
             return flightTime - 1;
@@ -26,7 +32,7 @@ public final class MissionControl {
         if (altitudes[0] - (flightTime - 1) >= altitudes[flightTime - 1]) {    // 고도가 내려가기만 하는 경우
             return 0;
         }
-        int searchResult = BinarySearch.binarySearch(altitudes);    // 올라갔다 내려가는경우 최대고도 찾기
+
         return searchResult;
     }
 
