35d302c85cd60870d6d2b851a2b67f1f7dae7794
dsadara <lim971230@yonsei.ac.kr>
2022-01-29 18:15:53 +0900

    Hash algorithm 체크하는 코드 추가

--- Lab4/src/academy/pocu/comp3500/lab4/Cracker.java
+++ Lab4/src/academy/pocu/comp3500/lab4/Cracker.java
@@ -3,6 +3,12 @@ package academy.pocu.comp3500.lab4;
 import academy.pocu.comp3500.lab4.pocuhacker.RainbowTable;
 import academy.pocu.comp3500.lab4.pocuhacker.User;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.zip.CRC32;
+
 
 public final class Cracker {
     final User[] userTable;
@@ -19,16 +25,82 @@ public final class Cracker {
         if (rainbowTables.length != 5)
             return null;
 
+        int hashAlgo = 0;   // 기본 해쉬 알고리즘은 CRC32로
+        String myPWHash;
+        byte[] inputBytes = password.getBytes(StandardCharsets.UTF_8);
+
+        // Check if Hash Algorithm is CRC32
+        CRC32 crc32 = new CRC32();
+        crc32.update(inputBytes);
+        myPWHash = String.valueOf(crc32.getValue());
+        for (User user: userTable) {
+            if (user.getPasswordHash().equals(myPWHash))
+                hashAlgo = 0;
+        }
+
+        // Check if Hash Algorithm is MD2
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD2");
+            md.update(inputBytes);
+            byte[] digest = md.digest();
+            myPWHash = Base64.getEncoder().encodeToString(digest);
+            for (User user: userTable) {
+                if (user.getPasswordHash().equals(myPWHash))
+                    hashAlgo = 1;
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+
+        // Check if Hash Algorithm is MD5
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(inputBytes);
+            byte[] digest = md.digest();
+            myPWHash = Base64.getEncoder().encodeToString(digest);
+            for (User user: userTable) {
+                if (user.getPasswordHash().equals(myPWHash))
+                    hashAlgo = 2;
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        // Check if Hash Algorithm is SHA1
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            md.update(inputBytes);
+            byte[] digest = md.digest();
+            myPWHash = Base64.getEncoder().encodeToString(digest);
+            for (User user: userTable) {
+                if (user.getPasswordHash().equals(myPWHash))
+                    hashAlgo = 3;
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        // Check if Hash Algorithm is SHA256
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(inputBytes);
+            byte[] digest = md.digest();
+            myPWHash = Base64.getEncoder().encodeToString(digest);
+            for (User user: userTable) {
+                if (user.getPasswordHash().equals(myPWHash))
+                    hashAlgo = 4;
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
         String[] plaintextPWs = new String[userTable.length];
         for (int i = 0; i < userTable.length; i++) {
                 String userPassWordHash = userTable[i].getPasswordHash();
-            for (int j = 0; j < 5; j++) {
-                if (rainbowTables[j].contains(userPassWordHash)) {
-                    plaintextPWs[i] = rainbowTables[j].get(userPassWordHash);
-                }
-            }
+                if (rainbowTables[hashAlgo].contains(userPassWordHash))
+                    plaintextPWs[i] = rainbowTables[hashAlgo].get(userPassWordHash);
         }
-
         return plaintextPWs;
     }
 }
