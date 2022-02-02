2b98dcffd7e763e586d9aaa45ba7a93197e487dc
DESKTOP-2K1CPFV\lim97 <lim971230@yonsei.ac.kr>
2022-02-02 20:10:25 +0900

    임시 업로드

--- Lab5/src/academy/pocu/comp3500/lab5/Bank.java
+++ Lab5/src/academy/pocu/comp3500/lab5/Bank.java
@@ -1,15 +1,30 @@
 package academy.pocu.comp3500.lab5;
 
-public class Bank {
-    public Bank(byte[][] pubKeys, final long[] amounts) {
+import java.util.HashMap;
 
+public class Bank {
+    private byte[][] pubKeys;
+    private long[] amounts;
+    HashMap<Integer, Integer> pubKeysHashMap = new HashMap<>();
+
+    public Bank(byte[][] pubKeys, final long[] amounts) {
+        this.pubKeys = pubKeys;
+        this.amounts = amounts;
+        // public key의 해시코드를 순서대로 배열에 저장
+        for (int i = 0; i < pubKeys.length; i++) {
+            int hashCode = pubKeys[i].hashCode();
+            pubKeysHashMap.put(hashCode, i);
+            System.out.println("pubKeysHashMap[" + i + "]: " + hashCode);
+        }
     }
 
     public long getBalance(final byte[] pubKey) {
-        return -1;
+        int index = pubKeysHashMap.get(pubKey.hashCode());
+        return amounts[index];
     }
 
     public boolean transfer(final byte[] from, byte[] to, final long amount, final byte[] signature) {
+
         return false;
     }
 }
