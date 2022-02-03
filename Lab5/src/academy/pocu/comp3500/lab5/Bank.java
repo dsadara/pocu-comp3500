6a7bab7d56f00d32502fc8e4f51b6791e18b339f
dsadara <lim971230@yonsei.ac.kr>
2022-02-04 00:34:24 +0900

    transfer 구현

--- Lab5/src/academy/pocu/comp3500/lab5/Bank.java
+++ Lab5/src/academy/pocu/comp3500/lab5/Bank.java
@@ -1,5 +1,17 @@
 package academy.pocu.comp3500.lab5;
 
+import academy.pocu.comp3500.lab5.app.Program;
+
+import javax.crypto.Cipher;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.RSAPublicKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 import java.util.HashMap;
 
 public class Bank {
@@ -14,7 +26,6 @@ public class Bank {
         for (int i = 0; i < pubKeys.length; i++) {
             int hashCode = pubKeys[i].hashCode();
             pubKeysHashMap.put(hashCode, i);
-            System.out.println("pubKeysHashMap[" + i + "]: " + hashCode);
         }
     }
 
@@ -23,8 +34,70 @@ public class Bank {
         return amounts[index];
     }
 
-    public boolean transfer(final byte[] from, byte[] to, final long amount, final byte[] signature) {
+    public boolean transfer(final byte[] from, final byte[] to, final long amount, final byte[] signature) {
+        String receivedMessageSHA256 = decryptWithPublicKey(signature, from);
+        if (receivedMessageSHA256 == null)
+            return false;
+        String validatingMessageSHA256;
 
-        return false;
+        ByteBuffer buffer = ByteBuffer.allocate(from.length + to.length + Long.BYTES);
+        buffer.put(from);
+        buffer.put(to);
+        buffer.putLong(amount);
+        byte[] message = buffer.array();
+
+        // sha256([from, to, amount])
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        md.update(message);
+        byte[] digest = md.digest();
+        validatingMessageSHA256 = Base64.getEncoder().encodeToString(digest);
+        if (!receivedMessageSHA256.equals(validatingMessageSHA256))
+            return false;
+
+        // transfer
+        int fromIndex = pubKeysHashMap.get(from.hashCode());
+        int toIndex = pubKeysHashMap.get(to.hashCode());
+        amounts[fromIndex] -= amount;
+        amounts[toIndex] += amount;
+
+
+        return true;
     }
+
+    private static String decryptWithPublicKey(final byte[] signature, final byte[] senderPublicKey) {
+        try {
+            PublicKey publicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(senderPublicKey));
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.DECRYPT_MODE, publicKey);
+            byte[] plaintext = cipher.doFinal(signature);
+            return Base64.getEncoder().encodeToString(plaintext);
+//            return new String(plaintext, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            return null;    // if sth wrong return null;
+        }
+    }
+
+    private static byte[] decodeFromHexString(String hexString) {
+        byte[] bytes = new byte[hexString.length() / 2];
+        for (int i = 0; i < hexString.length(); i += 2) {
+            int firstDigit = Character.digit(hexString.charAt(i), 16);
+            int secondDigit = Character.digit(hexString.charAt(i + 1), 16);
+            bytes[i / 2] = (byte) ((firstDigit << 4) + secondDigit);
+        }
+        return bytes;
+    }
+
+    private static String encodeToHexString(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte oneByte : bytes) {
+            result.append(String.format("%02x", oneByte));
+        }
+        return result.toString();
+    }
+
 }
