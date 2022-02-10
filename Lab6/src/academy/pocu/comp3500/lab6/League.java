db81781f06cd82a2e390e94827452b28429bd69a
dsadara <lim971230@yonsei.ac.kr>
2022-02-10 15:19:30 +0900

    G05

--- Lab6/src/academy/pocu/comp3500/lab6/League.java
+++ Lab6/src/academy/pocu/comp3500/lab6/League.java
@@ -129,7 +129,7 @@ public class League {
         if (!Node2.findRecursive(node, player))  // 이미 리그에 참여중인 선수가 아니라면 false 반환
             return false;
 
-        Node2 sudoparent = new Node2();
+        Node2 sudoparent = new Node2(new Player(Integer.MAX_VALUE, "^-^", Integer.MAX_VALUE));
         sudoparent.setLeft(node);
         Node2.deleteRecursive(node, player, sudoparent);
 
