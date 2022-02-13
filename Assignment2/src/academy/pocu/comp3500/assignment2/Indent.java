4df8b1030f5708a4f3f76e66b1c74cd4da350b92
dsadara <lim971230@yonsei.ac.kr>
2022-02-13 13:37:22 +0900

    text 저장방식 변경, Indent 리스트 추가

--- Assignment2/src/academy/pocu/comp3500/assignment2/Indent.java
+++ Assignment2/src/academy/pocu/comp3500/assignment2/Indent.java
@@ -1,27 +1,73 @@
 package academy.pocu.comp3500.assignment2;
 
+import academy.pocu.comp3500.assignment2.datastructure.LinkedList;
+
 import java.util.Iterator;
 
 public final class Indent {
-    final int indentLevel;
+    LinkedList<String> textList;
+    int indentLevel;
 
     public Indent(int indentLevel) {
+        textList = new LinkedList<>();
         this.indentLevel = indentLevel;
     }
 
-    public void discard() {
-        Iterator<ListElement> iter = Logger.list.iterator();
+    public void add(String text) {
+        for (int i = 0; i < indentLevel; i++) {
+            text = "  " + text;
+        }
+        textList.add(text);
+    }
 
-        while (iter.hasNext()) {
-            ListElement element = iter.next();
-            for (int i = indentLevel; i <= Logger.maxIndentLevel ; i++) {
-                if (element.indentLevel == i) {
-                    iter.remove();
-                }
+    public void discard() {
+//        Iterator<ListElement> iter = Logger.list.iterator();
+//
+//        while (iter.hasNext()) {
+//            ListElement element = iter.next();
+//            for (int i = indentLevel; i <= Logger.maxIndentLevel ; i++) {
+//                if (element.indentLevel == i) {
+//                    iter.remove();
+//                }
+//            }
+//        }
+//        if (indentLevel == 0)
+//            Logger.maxIndentLevel = 0;
+//        Logger.maxIndentLevel = indentLevel - 1;
+
+        Logger.currIndentLevel = this.indentLevel;
+
+        Iterator<Indent> indentIter = Logger.indentList.iterator();
+        Indent currIndent;
+        while (indentIter.hasNext()) {
+            currIndent = indentIter.next();
+            if (currIndent == this) {
+                break;
             }
         }
-        if (indentLevel == 0)
-            Logger.maxIndentLevel = 0;
-        Logger.maxIndentLevel = indentLevel - 1;
+
+        // 해당 indent 삭제
+        indentIter.remove();
+
+        // 삭제한 노드의 자식이 있으면 자식들도 삭제
+        Indent nextIndent;
+        if (indentIter.hasNext()) {     // 다음 노드 확인
+            nextIndent = indentIter.next();
+        }
+        else {
+            return;
+        }
+
+        if (nextIndent.indentLevel < this.indentLevel ) {   // 다음노드가 자식이라면
+            indentIter.remove();
+            while (indentIter.hasNext()) {
+                Indent indent = indentIter.next();
+                if (indent.indentLevel == this.indentLevel)
+                    break;
+                indentIter.remove();
+            }
+        }
+
+
     }
 }
--- Assignment2/src/academy/pocu/comp3500/assignment2/Logger.java
+++ Assignment2/src/academy/pocu/comp3500/assignment2/Logger.java
@@ -1,5 +1,6 @@
 package academy.pocu.comp3500.assignment2;
 
+import academy.pocu.comp3500.assignment2.datastructure.ArrayList;
 import academy.pocu.comp3500.assignment2.datastructure.LinkedList;
 
 import java.io.BufferedWriter;
@@ -7,74 +8,92 @@ import java.io.IOException;
 import java.util.Iterator;
 
 public final class Logger {
-    public static LinkedList<ListElement> list = new LinkedList<>();
+    public static LinkedList<Indent> indentList = new LinkedList<>();
+//    public static ArrayList<LinkedList<Indent>> parentList = new ArrayList<>();
     static int currIndentLevel = 0;
-    static int maxIndentLevel = 0;
 
     public static void log(final String text) {
-        int indentLevel = currIndentLevel;
-        list.addLast(new ListElement(text, indentLevel));
+        if (indentList.getSize() == 0) {    // indentList가 비어있으면
+            indentList.add(new Indent(currIndentLevel));
+        }
+
+        indentList.getLast().add(text);
     }
 
     public static void printTo(final BufferedWriter writer) {
-        Iterator<ListElement> iter = list.iterator();
+        // indent iterator
+        Iterator<Indent> indentIter = indentList.iterator();
         try {
-            while (iter.hasNext()) {
-                ListElement element = iter.next();
-                // indent 출력하기
-                for (int i = 0; i < element.indentLevel; i++) {
-                    writer.write("  ");
+            while (indentIter.hasNext()) {
+                Indent indent = indentIter.next();
+                // text iterator
+                Iterator<String> textIter = indent.textList.iterator();
+                while (textIter.hasNext()) {
+                    writer.write(textIter.next() + System.lineSeparator());
                 }
-                // text 출력하기
-                writer.write(element.text + System.lineSeparator());
             }
             writer.flush();
-            currIndentLevel = 0;
-            maxIndentLevel = 0;
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static void printTo(final BufferedWriter writer, final String filter) {
-        Iterator<ListElement> iter = list.iterator();
+//        Iterator<ListElement> iter = list.iterator();
+//        try {
+//            while (iter.hasNext()) {
+//                ListElement element = iter.next();
+//                if (element.text.contains(filter)) {
+//                    // indent 출력하기
+//                    for (int i = 0; i < element.indentLevel; i++) {
+//                        writer.write("  ");
+//                    }
+//                    // text 출력하기
+//                    writer.write(element.text + System.lineSeparator());
+//                }
+//            }
+//            writer.flush();
+//            currIndentLevel = 0;
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        // indent iterator
+        Iterator<Indent> indentIter = indentList.iterator();
         try {
-            while (iter.hasNext()) {
-                ListElement element = iter.next();
-                if (element.text.contains(filter)) {
-                    // indent 출력하기
-                    for (int i = 0; i < element.indentLevel; i++) {
-                        writer.write("  ");
-                    }
-                    // text 출력하기
-                    writer.write(element.text + System.lineSeparator());
+            while (indentIter.hasNext()) {
+                Indent indent = indentIter.next();
+                // text iterator
+                Iterator<String> textIter = indent.textList.iterator();
+                while (textIter.hasNext()) {
+                    String text = textIter.next();
+                    if (text.contains(filter))
+                        writer.write(text + System.lineSeparator());
                 }
             }
             writer.flush();
-            currIndentLevel = 0;
-            maxIndentLevel = 0;
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static void clear() {
-        list.clear();
-
+        indentList = new LinkedList<>();
+        currIndentLevel = 0;
     }
 
     public static Indent indent() {
         currIndentLevel++;
+        Indent newIndent = new Indent(currIndentLevel);
+        indentList.addLast(newIndent);
 
-        if (currIndentLevel > maxIndentLevel)
-            maxIndentLevel = currIndentLevel;
-
-        return new Indent(currIndentLevel);
+        return newIndent;
     }
 
     public static void unindent() {
         if (currIndentLevel == 0)
             return;
         currIndentLevel--;
+        Indent newIndent = new Indent(currIndentLevel);
+        indentList.addLast(newIndent);
     }
 }
