87c4b5ed894870d6b3dfd83045c619415a056162
DESKTOP-2K1CPFV\lim97 <lim971230@yonsei.ac.kr>
2022-03-18 15:32:12 +0900

    codingMan 구현

--- Lab9/src/academy/pocu/comp3500/lab9/CodingMan.java
+++ Lab9/src/academy/pocu/comp3500/lab9/CodingMan.java
@@ -6,42 +6,50 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class CodingMan {
+
+
     public static int findMinClipsCount(final VideoClip[] clips, int time) {
         // time만큼의 원소를 갖는 배열 생성
-        boolean[] timeArray = new boolean[time];
+        if (time == 0)
+            return -1;
+        boolean[] timeArray = new boolean[time + 1];
+
+        int[] occupyTime = new int[clips.length];
+        // videoClip에 OccupyTime 변수를 추가하여 boxing
+        VideoClipBoxed[] videoClipBoxed = new VideoClipBoxed[clips.length];
+        for (int i = 0; i < videoClipBoxed.length; i++) {
+            videoClipBoxed[i] = new VideoClipBoxed(clips[i], time);
+        }
+
+        Comparator<VideoClipBoxed> byOccupyTime = (VideoClipBoxed clip1, VideoClipBoxed clip2) -> Integer.compare(clip1.getOccupyTime(), clip2.getOccupyTime());
 
         // time 안에서 가장 많은 시간을 차지하는 순으로 정렬
-        Comparator<VideoClip> comparator = new Comparator<VideoClip>() {
-            @Override
-            public int compare(VideoClip o1, VideoClip o2) {
-                int o1Period = o1.getEndTime() - o1.getStartTime();
-                int o2Period = o2.getEndTime() - o2.getStartTime();
-                if (o1.getEndTime() > time) {
-                    o1Period = time - o1.getStartTime();
-                    System.out.println("o1Period : " + o1Period);
-                }
-                if (o2.getEndTime() > time) {
-                    o2Period = time - o2.getStartTime();
-                    System.out.println("o2Period : " + o2Period);
-                }
+        Arrays.sort(videoClipBoxed, byOccupyTime.reversed());
 
-                if (o1Period > o2Period) {
-                    return 1;
-                } else {
-                    return -1;
-                }
+        for (int i = 0; i < videoClipBoxed.length; i++) {
+            System.out.println("(" + videoClipBoxed[i].getVideoClip().getStartTime() + ", " + videoClipBoxed[i].getVideoClip().getEndTime() + ")");
+        }
+
+        int minClipNum = 0;
+        for (int i = 0; i < videoClipBoxed.length; i++) {
+            for (int j = videoClipBoxed[i].getVideoClip().getStartTime(); j <= videoClipBoxed[i].getVideoClip().getEndTime(); j++) {
+                timeArray[j] = true;
             }
-        };
-
-        Arrays.sort(clips, comparator);
-
-        System.out.println("time :" + time);
-        for (VideoClip clip : clips) {
-            System.out.println("(" + clip.getStartTime() + ", " + clip.getEndTime() + ")");
+            minClipNum++;
+            if (isTimeArrayFilled(timeArray)) {
+                return minClipNum;
+            }
         }
 
 
-
         return -1;
+    }
+
+    public static boolean isTimeArrayFilled(boolean[] timeArray) {
+        for (boolean b : timeArray) {
+            if (!b)
+                return false;
+        }
+        return true;
     }
 }
---
+++ Lab9/src/academy/pocu/comp3500/lab9/VideoClipBoxed.java
@@ -0,0 +1,36 @@
+package academy.pocu.comp3500.lab9;
+
+import academy.pocu.comp3500.lab9.data.VideoClip;
+
+public class VideoClipBoxed {
+    private VideoClip videoClip;
+    private int occupyTime;
+
+
+    public VideoClipBoxed(VideoClip videoClip, int time) {
+        if (time < videoClip.getEndTime()) {
+            this.occupyTime = time - videoClip.getStartTime();
+            this.videoClip = new VideoClip(videoClip.getStartTime(), time);
+        } else {
+            this.occupyTime = videoClip.getEndTime() - videoClip.getStartTime();
+            this.videoClip = videoClip;
+        }
+    }
+
+    public void setOccupyTime(int occupyTime) {
+        this.occupyTime = occupyTime;
+    }
+
+    public void setVideoClip(VideoClip videoClip) {
+        this.videoClip = videoClip;
+    }
+
+    public int getOccupyTime() {
+        return occupyTime;
+    }
+
+    public VideoClip getVideoClip() {
+        return videoClip;
+    }
+
+}
